fe9408e9ca8ccbf8c597f7125bd4c835d2fca858
liorv <lior.velichover@gmail.com>
2026-03-15 23:52:15 -0400

    removed dead code 1

--- packages/utils/src/invite.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function isNonEmptyString(value: unknown): value is string;
-export declare function isValidInviteToken(token: unknown): token is string;
-export declare function createInviteLink(origin: string, token: string): string;
-export declare function parseInviteTokenFromUrl(urlOrPath: string, fallbackOrigin?: string): string | null;
-//# sourceMappingURL=invite.d.ts.map
